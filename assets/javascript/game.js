23121b7124e3a6f2d63facf0b77730ad753ceaa5
MattScuteri <mttscuteri@gmail.com>
2017-12-16 16:26:27 -0500

    Jquery variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,26 +11,36 @@ $(document).ready(function() {
 		crystal1 = Math.floor(Math.random() * 10) + 1;
 		crystal2 = Math.floor(Math.random() * 10) + 1;
 		crystal3 = Math.floor(Math.random() * 10) + 1;
-		crystal4 = Math.floor(Math.random() * 10) + 1;	
-	}	
+		crystal4 = Math.floor(Math.random() * 10) + 1;
+		totalScore = 0;
+	};
+
+	beginGame();	
 
 	$("#crystal1").on("click", function() {
-
-	}
+		parseInt(crystal1);
+		totalScore = crystal1 + totalScore;
+	});
 
 	$("#crystal2").on("click", function() {
-		
-	}	
+		totalScore = crystal2 + totalScore
+	});	
 
 	$("#crystal3").on("click", function() {
-		
-	}	
+		totalScore = crystal3 + totalScore
+	});	
 
 	$("#crystal4").on("click", function() {
-		
-	}
+		totalScore = crystal4 + totalScore
+	});
 
-	
+	$("#randomNumber").html(randomNumber);
 
-	beginGame();
+	$("#totalScore").html(totalScore);
+
+	console.log(crystal1)
+	console.log(crystal2)
+	console.log(crystal3)
+	console.log(crystal4)
+
 });
