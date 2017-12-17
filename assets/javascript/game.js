a3ada26e784394ba777956e7b8f19add58d3b94f
MattScuteri <mttscuteri@gmail.com>
2017-12-17 01:21:53 -0500

    Final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,62 @@
 $(document).ready(function() {
-	
-	let randomNumber = Math.floor(Math.random() +102) + 19;
-	let totalScore = 0;
+
 	let wins = 0;
 	let losses = 0;
 
-	for (let i = 0; i < 4; i++) {
-		let crystalValues = Math.floor(Math.random() * 10) + 1;
-		console.log(crystalValues);
+	function resetGame() {
+		
+		$(".crystals").empty();
+		
+		randomNumber = Math.floor(Math.random() * 102) +19;
+		
+		totalScore = 0;
+		
+		for (let i = 0; i < 4; i++) {
+			let crystalValues = Math.floor(Math.random() * 10) + 1;
+			 	console.log(crystalValues);
 
-		let crystal = $("<img>");
-		crystal.attr({
-			"class" : 'crystal',
-			"data-crystalvalue" : crystalValues
-		});
+			let crystal = $("<img>");
+			crystal.attr({
+				"class" : 'crystal',
+				"data-crystalvalue" : crystalValues
+			});
 
-		$(".crystals").append(crystal);
+			$(".crystals").append(crystal);
 
-		crystal.attr({"src": "assets/images/crystal1.jpg",
-			"src": "assets/images/crystal2.jpg",
-			"src": "assets/images/crystal3.png",
-			"src": "assets/images/crystal4.jpg"
-		});
+			crystal.attr({"src": "assets/images/crystal1.jpg",
+				"src": "assets/images/crystal2.jpg",
+				"src": "assets/images/crystal3.png",
+				"src": "assets/images/crystal4.jpg"
+			});
+		};
 	};
 
-	$(".crystal").on("click", function() { 
+	resetGame();
+
+	$("#randomNumber").html(randomNumber);
+
+	$(document).on("click", ".crystal", function() { 
 		
 		let crystalResult = parseInt(($(this).attr('data-crystalvalue')));
 
 		totalScore += crystalResult
 
 		$("#totalScore").html(totalScore);
+	
+		if (totalScore === randomNumber) {
+			wins++;
+			$("#wins").html("Wins: " + wins)
+			totalScore = 0;
 
-		console.log(totalScore);
-		});
+			resetGame();
 
-	$("#randomNumber").html(randomNumber);
+		} else if (totalScore > randomNumber) {
+			losses++;
+			$("#losses").html("Losses: " + losses)			
+			totalScore = 0;
+
+			resetGame();
+		}
+	});
 
 });
